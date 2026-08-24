0ca454a571905607527f114ac03736a294fc819d
Suraj Kumar Shah <surajks.shah@gmail.com>
2026-08-24 11:33:32 +0545

    finak

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: globalThis.process?.env?.BASE_PATH || '/Electrohub/',
+  base: globalThis.process?.env?.BASE_PATH || '/Electrohub/dist/',
   plugins: [react(), tailwindcss()],
 })
