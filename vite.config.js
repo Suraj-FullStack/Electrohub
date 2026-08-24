89ddeab1774d17662b5f8576fbc09c126be45a91
Suraj Kumar Shah <surajks.shah@gmail.com>
2026-08-24 11:19:12 +0545

    dgxfytchgh

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: globalThis.process?.env?.BASE_PATH || '/',
+  base: globalThis.process?.env?.BASE_PATH || '/Electrohub/',
   plugins: [react(), tailwindcss()],
 })
